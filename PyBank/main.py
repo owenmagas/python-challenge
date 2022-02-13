fbd2194401d01bfc97f47aa216f7e9a298eab182
owenmagazine <owenmagas@owenmagazines-MacBook-Pro.local>
2022-02-13 14:10:47 +0200

    Final Work

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,53 @@
-import sys
-import pandas as pd
-import numpy as np
+import csv
+from os import path
 
-df = pd.read_csv("/Users/owenmagas/Documents/MDS/Level 1.1/Advanced Prtogramming For Data Analytics/"
-                 "python-challenge/PyBank/Resources/budget_data.csv")
-# print(df.head())
+basepath=path.dirname(__file__)
+filepath = path.abspath(path.join(basepath,'Resources/budget_data.csv'))
+filepath1 = path.abspath(path.join(basepath,'analysis/text_file.txt'))
+with open(filepath, 'r') as ed:
+    csv_reader = csv.reader(ed)
+    next(csv_reader)
+    date=[]
+    P_L=[]
+   
+    for line in csv_reader:
+        date.append(line[0])
+        P_L.append(int(line[1]))
+       
+    
+    # print(P_L)
+    
+    total = 0
+    for i in P_L:
+        total+=(i)
 
-# df.set_index("Date", inplace=True)
-number_of_months = df['Date'].unique()
-# print(len(number_of_months))
-#print(df.info())
-#print(df['Profit/Losses'].sum())
+    
+    change  = []
+    for i in range(len(P_L)):
+        if i == (len(P_L)-1):
+            break
+        change.append(P_L[i+1]-P_L[i])
+    change.insert(0,0)
 
 
+
+    average_change = sum(change)/85
+
+    high = max(change)
+    low = min(change)
+    high_index = change.index(high)
+    low_index  = change.index(low)
+    
+    print("Financial Analysis\n -------------------------\n "
+        "Total Months:{} \n Total: ${}\n Average Change: ${:.2f}\n"
+         " Greatest Increase in Profits: {} (${}) \n "
+        "Greatest Decrease in Profits: {} (${})".format(len(date),total,
+        average_change, date[high_index],high, date[low_index], low))
+    
+    
+    with open(filepath1, 'w') as o:
+        o.write("Financial Analysis\n -------------------------\n "
+        "Total Months:{} \n Total: ${}\n Average Change: ${:.2f}\n"
+         " Greatest Increase in Profits: {} (${}) \n "
+        "Greatest Decrease in Profits: {} (${})".format(len(date),total,
+        average_change, date[high_index],high, date[low_index], low))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,11 +1,64 @@
-# import sys
-import pandas as pd
-# import numpy as np
+import csv
+from os import path
 
-df = pd.read_csv("/Users/owenmagas/Documents/MDS/Level 1.1/Advanced Prtogramming For Data Analytics/"
-                 "python-challenge/PyPoll/Resources/election_data.csv")
-# print(df.head())
+basepath=path.dirname(__file__)
+filepath = path.abspath(path.join(basepath,'Resources/election_data.csv'))
+filepath1 = path.abspath(path.join(basepath,'analysis/text_file.txt'))
+with open(filepath, 'r') as ed:
+    csv_reader = csv.reader(ed)
+    next(csv_reader)
+    voter_id=set()
+    county=[]
+    candidate=[]
+    for line in csv_reader:
+        voter_id.add(line[0])
+        county.append(line[1])
+        candidate.append(line[2])
+    
+    
+    cand = set(candidate)
+    dict={}
+    for i in cand:
+        dict[i] = candidate.count(i) 
+        #print('{}: {}'. format(i, candidate.count(i)))
+        # print(dict)
+    
+    def get_key(val):
+        for key, value in dict.items():
+            if val== value:
+                return key
+        return 'key does not exist'
+    
+    khan = dict['Khan']
+    correy = dict['Correy']
+    li = dict['Li']
+    otooley = dict["O'Tooley"]
+    khan_perc = dict['Khan']/len(voter_id)*100
+    correy_perc = dict['Correy']/len(voter_id)*100
+    li_perc = dict['Li']/len(voter_id)*100
+    otooley_perc = dict["O'Tooley"]/len(voter_id)*100
+    print("Election Results\n---------------------------------\n"
+    "Total Votes: {}\n"
+    "Khan: {:.4f}% ({})\n"
+    "Correy: {:.4f}% ({})\n"
+    "Li: {:.4f}% ({})\n"
+    "O'Tooley: {:.4f}% ({})\n"
+    "---------------------------------\n"
+    "Winner: {}\n"
+    "---------------------------------". format(len(voter_id), khan_perc, khan,
+    correy_perc, correy, li_perc, li, otooley_perc, otooley,
+    get_key(max(dict.values()))))
 
-df.set_index("Voter ID", inplace=True)
-
-print(df.head())
+    with open(filepath1, 'a') as o:
+        o.write("Election Results\n---------------------------------\n"
+    "Total Votes: {}\n"
+    "Khan: {:.4f}% ({})\n"
+    "Correy: {:.4f}% ({})\n"
+    "Li: {:.4f}% ({})\n"
+    "O'Tooley: {:.4f}% ({})\n"
+    "---------------------------------\n"
+    "Winner: {}\n"
+    "---------------------------------". 
+    format(len(voter_id), khan_perc, khan,
+    correy_perc, correy, li_perc, li, otooley_perc, otooley,
+    get_key(max(dict.values()))))
